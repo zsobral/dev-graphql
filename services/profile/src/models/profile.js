560affc3bbe63f9ca0845fc2586159fb77c2c508
Mateus Sobral <mateus.sobral.monteiro@gmail.com>
2020-05-05 05:15:08 -0300

    fix profile model schema

--- services/profile/src/models/profile.js
+++ services/profile/src/models/profile.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const profileSchema = new mongoose.Schema({
-  accountId: { type: String, required: true },
+  accountId: { type: String, required: true, unique: true },
   avatar: { type: String },
   fullName: { type: String, trim: true },
   username: { type: String, required: true, unique: true },
